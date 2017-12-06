5e3b208664d2baa413d086bf475dae93cbebe7aa
Yura <rager.tf@gmail.com>
2017-12-07 00:07:23 +0300

    zhri svoi sraniy service

--- app/src/main/java/com/example/ruslanio/keyboard/TransmitterService.java
+++ app/src/main/java/com/example/ruslanio/keyboard/TransmitterService.java
@@ -26,18 +26,16 @@ import retrofit2.Response;
  * Created by Ruslanio on 30.11.2017.
  */
 
-public class TransmitterService extends Service {
+public class TransmitterService extends IntentService {
     private static final int AWAIT_TIME = 5000;
-
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
 
     private DBHelper mDBHelper;
     private ApiManager mApiManager;
     private boolean mIsRunning;
+
+    public TransmitterService() {
+        super("KeyService");
+    }
 
     @Override
     public void onCreate() {
@@ -45,64 +43,6 @@ public class TransmitterService extends Service {
         System.out.println("onCreate()");
         mDBHelper = new DBHelper(getApplicationContext());
         mApiManager = ApiManager.getInstance();
-    }
-
-    @SuppressLint("WrongConstant")
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Observable.<Integer>fromCallable(() -> {
-            if (!mIsRunning) {
-                mIsRunning = true;
-                while (mIsRunning) {
-                    try {
-                        SQLiteDatabase db = mDBHelper.getWritableDatabase();
-                        Cursor cursor = db.query(DBHelper.TEXT_ENTITY_TABLE_NAME, null,
-                                DBHelper.TextEntityTable.TEXT_ENTITY_STATUS + " = " + DBHelper.STATUS_CLIENT,
-                                null, null, null, null);
-
-
-                        if (cursor.moveToFirst()) {
-                            int id = cursor.getColumnIndex(DBHelper.TextEntityTable.TEXT_ENTITY_ID);
-                            int text = cursor.getColumnIndex(DBHelper.TextEntityTable.TEXT_ENTITY_TEXT);
-
-                            do {
-                                int currentId = cursor.getInt(id);
-                                String currentText = cursor.getString(text);
-
-                                ContentValues contentValues = new ContentValues();
-                                contentValues.put(DBHelper.TextEntityTable.TEXT_ENTITY_ID, currentId);
-                                contentValues.put(DBHelper.TextEntityTable.TEXT_ENTITY_TEXT, currentText);
-                                contentValues.put(DBHelper.TextEntityTable.TEXT_ENTITY_STATUS, DBHelper.STATUS_SERVER);
-
-
-//                        AddDataRequestBody requestBody = new AddDataRequestBody(currentText);
-                                Call<EmptyResult> call = mApiManager.postData(currentText);
-                                Response response = call.execute();
-                                if (response.isSuccessful()) {
-
-                                    System.out.println(response.code());
-                                    db.update(DBHelper.TEXT_ENTITY_TABLE_NAME, contentValues,
-                                            DBHelper.TextEntityTable.TEXT_ENTITY_ID + " = " + currentId, null);
-                                } else
-                                    System.out.println("SOMETHING WITH INTERNET");
-
-                            } while (cursor.moveToNext());
-                        }
-                        Thread.sleep(AWAIT_TIME);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        System.out.println("SOMETHING WITH THREADS");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println("SOMETHING WITH CODE");
-                    }
-                }
-            }
-            return 1;
-        })
-                .subscribeOn(Schedulers.newThread())
-                .subscribe();
-        return START_STICKY;
     }
 
     @Override
@@ -116,5 +56,54 @@ public class TransmitterService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        while (mIsRunning) {
+            mIsRunning=true;
+            try {
+                SQLiteDatabase db = mDBHelper.getWritableDatabase();
+                Cursor cursor = db.query(DBHelper.TEXT_ENTITY_TABLE_NAME, null,
+                        DBHelper.TextEntityTable.TEXT_ENTITY_STATUS + " = " + DBHelper.STATUS_CLIENT,
+                        null, null, null, null);
+
+
+                if (cursor.moveToFirst()) {
+                    int id = cursor.getColumnIndex(DBHelper.TextEntityTable.TEXT_ENTITY_ID);
+                    int text = cursor.getColumnIndex(DBHelper.TextEntityTable.TEXT_ENTITY_TEXT);
+
+                    do {
+                        int currentId = cursor.getInt(id);
+                        String currentText = cursor.getString(text);
+
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put(DBHelper.TextEntityTable.TEXT_ENTITY_ID, currentId);
+                        contentValues.put(DBHelper.TextEntityTable.TEXT_ENTITY_TEXT, currentText);
+                        contentValues.put(DBHelper.TextEntityTable.TEXT_ENTITY_STATUS, DBHelper.STATUS_SERVER);
+
+
+//                        AddDataRequestBody requestBody = new AddDataRequestBody(currentText);
+                        Call<EmptyResult> call = mApiManager.postData(currentText);
+                        Response response = call.execute();
+                        if (response.isSuccessful()) {
+
+                            System.out.println(response.code());
+                            db.update(DBHelper.TEXT_ENTITY_TABLE_NAME, contentValues,
+                                    DBHelper.TextEntityTable.TEXT_ENTITY_ID + " = " + currentId, null);
+                        } else
+                            System.out.println("SOMETHING WITH INTERNET");
+
+                    } while (cursor.moveToNext());
+                }
+                Thread.sleep(AWAIT_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("SOMETHING WITH THREADS");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("SOMETHING WITH CODE");
+            }
+        }
     }
 }
